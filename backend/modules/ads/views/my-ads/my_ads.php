1a6e3907a98d435f54f9cc948a51a014b0b4ddc8
akbarjoudi <akbar.joody@gmail.com>
2017-12-06 10:18:49 +0330

    init

--- backend/modules/ads/views/my-ads/my_ads.php
+++ backend/modules/ads/views/my-ads/my_ads.php
@@ -6,30 +6,47 @@ use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\widgets\Pjax;
 ?>
-<?= Html::a('<i></i> ثبت تبلیغ جدید', Url::to(['new-ads']), [
-    'class' => 'btn btn-sm btn-success',
-    'onclick' => "showModal(this); return false;",
-]); ?>
+
 <div class="row">
-<?php foreach ($adsModel as $ads):?>
-    <div class="col-lg-4 col-md-3">
-    <?php  ?>
-    <?= Card::widget([
-        'ads' => $ads,
-        'buttons' => [
-            'update' => function($widget)use($ads){
-                return Html::a('<i class="fa fa-edit"></i>', Url::to(['edit-ads', 'aid' => $ads->id]), [
+    <div class="col-lg-8 col-md-8 col-lg-offset-4 col-md-offset-4">
+         <?php if(empty($adsModel)){ ?>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <i class="fa fa-angle-left"></i> عدم وجود تبلیغ
+            </div>
+            <div class="panel-body">
+                <div style="padding: 100px 100px 0px 0px;color: blueviolet;font-family: tahoma;font-size: xx-large;background: url('themes/adminlte/img/alert-ads.png');background-size: cover; height: 400px; opacity: 1; width:500px;" class="pull-left animated bounceIn">
+                    تبلیغی وجود ندارد
+                </div>
+                <p>شما در حال حاضر هیچ تبلیغی نداردید</p>
+                برای در ج تبلیغ شروع کنید
+                <?= Html::a('<i class="fa fa-plus"></i> ثبت تبلیغ جدید', Url::to(['new-ads']), [
+                    'class' => 'btn btn-sm btn-success',
                     'onclick' => "showModal(this); return false;",
-                ]);
-            },
-            'view' => function($widget)use($ads){
-                return Html::a('<i class="fa fa-eye"></i>', Url::to(['view-myads', 'aid' => $ads->id]));
-            }
-        ]
-    ]); ?>
+                ]); ?>
+                
+            </div>
+        </div>
+        <?php } ?>
+        <?php foreach ($adsModel as $ads):?>
+                    <div class="col-lg-4 col-md-3">
+                    <?php  ?>
+                    <?= Card::widget([
+                        'ads' => $ads,
+                        'buttons' => [
+                            'update' => function($widget)use($ads){
+                                return Html::a('<i class="fa fa-edit"></i>', Url::to(['edit-ads', 'aid' => $ads->id]), [
+                                    'onclick' => "showModal(this); return false;",
+                                ]);
+                            },
+                            'view' => function($widget)use($ads){
+                                return Html::a('<i class="fa fa-eye"></i>', Url::to(['view-myads', 'aid' => $ads->id]));
+                            }
+                        ]
+                    ]); ?>
+                    </div>
+                <?php endforeach; ?>
     </div>
-<?php endforeach; ?>
-    
 </div>
 <?php 
 Modal::begin(['id' => 'my-ads-modal']);
